14aff6b438be57bab0902d287c30e8229144ba25
Khariton90 <82729640+Khariton90@users.noreply.github.com>
2022-05-12 17:04:52 +0300

    Революция или эволюция? (часть 2) (1.1)

--- src/model/film-model.js
+++ src/model/film-model.js
@@ -1,9 +1,12 @@
 import { generateComment } from '../mock/comment';
 import { generateFilm } from '../mock/film';
 
+const FILMS_MAX_LENGTH = 22;
+const COMMENTS_MAX_LENGTH = 10;
+
 export default class FilmsModel {
-  #films = Array.from({ length: 22 }, generateFilm);
-  #comments = Array.from({ length: 10 }, generateComment);
+  #films = Array.from({ length: FILMS_MAX_LENGTH }, generateFilm);
+  #comments = Array.from({ length: COMMENTS_MAX_LENGTH }, generateComment);
 
   get films() {
     return this.#films;
